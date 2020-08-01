5ffa8b878915db04e6fee71cd705cf50fa7e5c2d
Caleb335 <belac335@gmail.com>
2020-08-01 10:46:05 +0100

    allowing http access control

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -28291,17 +28291,100 @@ if ("development" === 'production') {
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = App;
+exports.default = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function App() {
-  return _react.default.createElement("div", {
-    className: "app__base"
-  }, _react.default.createElement("h1", null, "Welcome!"));
-}
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+// query parameters
+var moneyValue = '506.54';
+var currency = 'NGN';
+var language = 'en'; // api globals
+
+var api_key = 'tuforty_159FvsJ263hXlcbEJVCpxvW9VqWm9JBRF06uhuhMoeAELDuTEMGjdFUvgHxn';
+var tuforty_endpoint = "https://tuforty.com/api/v1/translator/money?value=".concat(moneyValue, "&language=").concat(language, "&currency=").concat(currency);
+var dataParams = {
+  method: 'GET',
+  mode: 'no-cors',
+  headers: {
+    'content-type': 'application/json; charset=UTF-8',
+    Authorization: "Bearer ".concat(api_key),
+    'Access-Control-Allow-Origin': 'http://localhost:1234/'
+  }
+};
+
+var App = /*#__PURE__*/function (_React$Component) {
+  _inherits(App, _React$Component);
+
+  var _super = _createSuper(App);
+
+  function App(props) {
+    var _this;
+
+    _classCallCheck(this, App);
+
+    _this = _super.call(this, props);
+    _this.state = {
+      translatedResult: [],
+      isLoading: false
+    };
+    return _this;
+  }
+
+  _createClass(App, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      var isLoading = this.state.isLoading;
+
+      if (isLoading) {
+        return _react.default.createElement("p", null, "Loading ...");
+      } // http request
+
+
+      fetch(tuforty_endpoint, dataParams).then(function (data) {
+        return data.json();
+      }).then(function (response) {
+        console.log(response);
+      }).catch(function (error) {
+        console.log(error);
+      });
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      return _react.default.createElement("div", {
+        className: "app__base"
+      }, _react.default.createElement("h1", null, "Welcome"));
+    }
+  }]);
+
+  return App;
+}(_react.default.Component);
+
+exports.default = App;
 },{"react":"../node_modules/react/index.js"}],"../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
 var bundleURL = null;
 
@@ -28416,7 +28499,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44981" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45941" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,60 @@
 import React from 'react'
 
-export default function App() {
-  return (
-    <div className="app__base">
-      <h1>Welcome!</h1>
-    </div>
-  )
+// query parameters
+const moneyValue = '506.54'
+const currency = 'NGN'
+const language = 'en'
+
+// api globals
+const api_key =
+  'tuforty_159FvsJ263hXlcbEJVCpxvW9VqWm9JBRF06uhuhMoeAELDuTEMGjdFUvgHxn'
+const tuforty_endpoint = `https://tuforty.com/api/v1/translator/money?value=${moneyValue}&language=${language}&currency=${currency}`
+
+const dataParams = {
+  method: 'GET',
+  mode: 'no-cors',
+  headers: {
+    'content-type': 'application/json; charset=UTF-8',
+    Authorization: `Bearer ${api_key}`,
+    'Access-Control-Allow-Origin': 'http://localhost:1234/'
+  },
+}
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      translatedResult: [],
+      isLoading: false,
+    }
+  }
+
+  componentDidMount() {
+    const { isLoading } = this.state
+
+    if (isLoading) {
+      return <p>Loading ...</p>
+    }
+
+    // http request
+
+    fetch(tuforty_endpoint, dataParams)
+      .then((data) => {
+        return data.json()
+      })
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  render() {
+    return (
+      <div className="app__base">
+        <h1>Welcome</h1>
+      </div>
+    )
+  }
 }
